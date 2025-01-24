22408bf3f587879cd4fa15b5983d7dcd9c1fe9f9
Pierrick <pierrick.catteau@gmail.com>
2025-01-24 11:01:14 +0100

    seperated file for cards declarations

---
+++ game/cards.lua
@@ -0,0 +1,64 @@
+local cardDefinitions = {
+    -- Duo cards
+    -- Crabs
+    {type = "crab", count = 2, color = "dark blue", nature = "duo"},
+    {type = "crab", count = 2, color = "light blue", nature = "duo"},
+    {type = "crab", count = 2, color = "yellow", nature = "duo"},
+    {type = "crab", count = 1, color = "gray", nature = "duo"},
+    {type = "crab", count = 1, color = "green", nature = "duo"},
+    {type = "crab", count = 1, color = "black", nature = "duo"},
+    -- Boats
+    {type = "boat", count = 2, color = "dark blue", nature = "duo"},
+    {type = "boat", count = 2, color = "light blue", nature = "duo"},
+    {type = "boat", count = 2, color = "yellow", nature = "duo"},
+    {type = "boat", count = 2, color = "black", nature = "duo"},
+    -- Fishes
+    {type = "fish", count = 2, color = "dark blue", nature = "duo"},
+    {type = "fish", count = 2, color = "black", nature = "duo"},
+    {type = "fish", count = 1, color = "light blue", nature = "duo"},
+    {type = "fish", count = 1, color = "yellow", nature = "duo"},
+    {type = "fish", count = 1, color = "green", nature = "duo"},
+    -- Swimmers
+    {type = "swimmer", count = 1, color = "dark blue", nature = "duo"},
+    {type = "swimmer", count = 1, color = "light blue", nature = "duo"},
+    {type = "swimmer", count = 1, color = "yellow", nature = "duo"},
+    {type = "swimmer", count = 1, color = "black", nature = "duo"},
+    {type = "swimmer", count = 1, color = "orange", nature = "duo"},
+    -- Sharks
+    {type = "shark", count = 1, color = "dark blue", nature = "duo"},
+    {type = "shark", count = 1, color = "light blue", nature = "duo"},
+    {type = "shark", count = 1, color = "black", nature = "duo"},
+    {type = "shark", count = 1, color = "purple", nature = "duo"},
+    {type = "shark", count = 1, color = "green", nature = "duo"},
+    -- Mermaids
+    {type = "mermaid", count = 4, color = "white", nature = "duo"},
+    -- Collector cards
+    -- Shells
+    {type = "shell", count = 1, color = "dark blue", nature = "collection"},
+    {type = "shell", count = 1, color = "light blue", nature = "collection"},
+    {type = "shell", count = 1, color = "green", nature = "collection"},
+    {type = "shell", count = 1, color = "gray", nature = "collection"},
+    {type = "shell", count = 1, color = "black", nature = "collection"},
+    {type = "shell", count = 1, color = "yellow", nature = "collection"},
+    -- Octopuses
+    {type = "octopus", count = 1, color = "light blue", nature = "collection"},
+    {type = "octopus", count = 1, color = "purple", nature = "collection"},
+    {type = "octopus", count = 1, color = "gray", nature = "collection"},
+    {type = "octopus", count = 1, color = "green", nature = "collection"},
+    {type = "octopus", count = 1, color = "yellow", nature = "collection"},
+    -- Penguins
+    {type = "penguin", count = 1, color = "purple", nature = "collection"},
+    {type = "penguin", count = 1, color = "pink", nature = "collection"},
+    {type = "penguin", count = 1, color = "orange", nature = "collection"},
+    -- Sailors
+    {type = "sailor", count = 1, color = "ocre", nature = "collection"},
+    {type = "sailor", count = 1, color = "pink", nature = "collection"},
+    -- Point Multiplier cards
+    {type = "lighthouse", count = 1, color = "purple", nature = "mult"},
+    {type = "shoal", count = 1, color = "gray", nature = "mult"},
+    {type = "colony", count = 1, color = "green", nature = "mult"},
+    {type = "captain", count = 1, color = "orange", nature = "mult"},
+}
+
+-- Retourne la table cardDefinitions
+return cardDefinitions
